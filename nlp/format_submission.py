9f3b5861635e2d6f23eb8259c73c5226c1f3fb66
HHansi <h.hansi1991@gmail.com>
2022-12-20 20:27:08 +0000

    004: Adding nlp format

---
+++ nlp/format_submission.py
@@ -0,0 +1,19 @@
+# Created by Hansi at 12/16/2022
+import json
+
+import pandas as pd
+
+test_file_path = "data/test.csv"
+df_test = pd.read_csv(test_file_path)
+df_test = df_test[["id", "prediction"]]
+
+data = []
+for index, row in df_test.iterrows():
+    data.append({'id': row['id'], 'prediction': row['prediction']})
+
+print(data[0:5])
+
+submission_file_path = "submission.json"
+with open(submission_file_path, 'w') as fp:
+    fp.write('\n'.join(json.dumps(i) for i in data))
+
